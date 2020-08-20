acc2e1eaac7a2693e685db6f73dd8dcc6154051a
Ryan <rlorenz@uco.edu>
2020-08-19 20:39:58 -0500

    JFrame window added

--- Lesson0/Main.java
+++ Lesson0/Main.java
@@ -1,7 +1,15 @@
 package Lesson0;
 
+import javax.swing.JFrame;
+
 public class Main {
 	public static void main(String[] args) {
 
+		JFrame window = new JFrame();
+		window.setSize(5000, 3000);
+		window.setLocation(400, 200);
+
+		window.pack();
+		window.setVisible(true);
 	}
 }
